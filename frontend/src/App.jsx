8e8e230e70563ae67720a17920a2ec92742e0377
Hafid Siraj Aurakhmah Witra <hafidsiraj.aw@gmail.com>
2024-03-29 11:15:41 +0700

    initial frotend with reactjs

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className='bg-red-400 text-white'>
+                App
+            </div>
+        );
+    }
+}
+
+export default App;
