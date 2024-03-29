9d81cc55edb8e276f60a5d020138da2ed9d8c09c
Hafid Siraj Aurakhmah Witra <hafidsiraj.aw@gmail.com>
2024-03-29 16:35:48 +0700

    [FEATURE]show form add new flower

--- frontend/src/pages/CreateFlower.jsx
+++ frontend/src/pages/CreateFlower.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
+import BackButton from "../components/BackButton.jsx";
+import Spin from "../components/Spin.jsx";
 
 const CreateFlower = () => {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [stock, setStock] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const navigate = useNavigate();
+    const handeSaveFLower = () => {
+        const data = {
+            name,
+            description,
+            price,
+            stock
+        };
+        setLoading(true);
+        axios
+            .post('http://localhost:8080/flowers', data)
+            .then(() => {
+                setLoading(false);
+                navigate('/');
+            })
+            .catch((error) => {
+                setLoading(false);
+                console.log(error);
+            })
+    };
+
     return (
-        <div>
-            
+        <div className='p-4'>
+            <BackButton/>
+            <h1 className='text-3xl my-4'>Add new Flower</h1>
+            {loading ? <Spin/> : ''}
+            <div className='flex flex-col border-2 boder-sky-4000 rounded-xl w-[600px] p-4 mx-auto'>
+                <div className='my-4'>
+                    <label className='text-xl mr-4 text-gray-500'>Name</label>
+                    <input type='text' value={name} onChange={(e) => setName(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full'/>
+                    <label className='text-xl mr-4 text-gray-500'>Description</label>
+                    <input type='text' value={description} onChange={(e) => setDescription(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full'/>
+                    <label className='text-xl mr-4 text-gray-500'>Price</label>
+                    <input type='number' value={price} onChange={(e) => setPrice(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full'/>
+                    <label className='text-xl mr-4 text-gray-500'>Stock</label>
+                    <input type='number' value={stock} onChange={(e) => setStock(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full'/>
+                </div>
+                <button className='p-2 bg-sky-300 m-8' onClick={handeSaveFLower}>
+                    Save
+                </button>
+            </div>
         </div>
     );
 };
