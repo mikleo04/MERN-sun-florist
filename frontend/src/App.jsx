ebf165bf8f7c8fb0b69611331069aebbf9124680
Hafid Siraj Aurakhmah Witra <hafidsiraj.aw@gmail.com>
2024-03-30 10:35:59 +0700

    [FEATURE] add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,14 @@ import ShowFlower from "./pages/admin/ShowFlower.jsx";
 import CreateFlower from "./pages/admin/CreateFlower.jsx";
 import UpdateFLower from "./pages/admin/UpdateFLower.jsx";
 import DeleteFlower from "./pages/admin/DeleteFlower.jsx";
+import HomePage from "./pages/client/HomePage.jsx";
 
 class App extends Component {
     render() {
         return (
             <Routes>
-                <Route path='/' element={ <Home/> } />
+                <Route path='/' element={ <HomePage/> } />
+                <Route path='/admin' element={ <Home/> } />
                 <Route path='/flowers/details/:id' element={ <ShowFlower/> } />
                 <Route path='/flowers/create' element={ <CreateFlower/> } />
                 <Route path='/flowers/update/:id' element={ <UpdateFLower/> } />
