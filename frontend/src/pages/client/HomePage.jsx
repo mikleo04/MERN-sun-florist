7314cc4a5862c2bcfbe8d06aa4ff0a0582bac637
Hafid Siraj Aurakhmah Witra <hafidsiraj.aw@gmail.com>
2024-03-30 14:30:39 +0700

    [IMPROVE] update ui home page client

--- frontend/src/pages/client/HomePage.jsx
+++ frontend/src/pages/client/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import NavBar from "../../components/NavBar.jsx";
+import ListFlower from "./components/ListFlower.jsx";
+import Footer from "../../components/Footer.jsx";
 
 const HomePage = () => {
     return (
         <>
             <NavBar/>
+            <ListFlower/>
+            <Footer/>
         </>
     );
 };
